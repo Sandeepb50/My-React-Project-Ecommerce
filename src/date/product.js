86d1f12ed497395f86c703b55716c90a05101d93
Sandeep50 <sandeep50badiger@gmail.com>
2025-05-15 11:36:29 +0530

    added

--- src/date/product.js
+++ src/date/product.js
@@ -1,8 +1,8 @@
 import product1 from "../assets/images/images3.png";
 import product2 from "../assets/images/im1.png";
-import product3 from "../assets/images/im1.png";
-import product4 from "../assets/images/image4.png";
-import product5 from "../assets/images/img5.png";
+import product3 from "../assets/images/headphone.png";
+import product4 from "../assets/images/jacket.png";
+import product5 from "../assets/images/Shadow cap.png";
 
 export const products = [
   {
